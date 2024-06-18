aa2c77c74221b79acacc54f049300d509819ab6e
aiken-yao <120625622+aiken-yao@users.noreply.github.com>
2024-06-18 16:11:39 +0800

    add loved label

--- entity.py
+++ entity.py
@@ -1,5 +1,6 @@
 class Image:
-    def __init__(self, name, note="", label="no label"):
+    def __init__(self, name, note="", loved="False", label="no label"):
         self.name = name
         self.note = note
         self.label = label
+        self.loved = loved
